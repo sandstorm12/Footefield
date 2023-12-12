e6d92cf1646cbe2568a5487ac9d583e258097290
sandstorm12 <sandstormeatwo@gmail.com>
2023-12-12 16:26:00 +0330

    A draft of extrinsic error calculation code is added, however the error is high, more tinkering is needed

---
+++ calc_extrinsic_error.py
@@ -0,0 +1,141 @@
+import cv2
+import diskcache
+import numpy as np
+
+import detect_chessboard
+
+from tqdm import tqdm
+
+
+def get_obj_points():
+    cols = detect_chessboard.CHESSBOARD_COLS
+    rows = detect_chessboard.CHESSBOARD_ROWS
+    square_size = detect_chessboard.CHESSBOARD_SQRS
+
+    obj_points = np.zeros((cols * rows, 3), np.float32)
+    obj_points[:, :2] = np.mgrid[0:cols, 0:rows].T.reshape(-1, 2) * square_size
+
+    return obj_points
+
+
+def get_all_keys(cache):
+    keys_all = cache._sql('SELECT key FROM Cache').fetchall()
+    keys_all = [item[0] for item in keys_all]
+
+    return keys_all
+
+
+def load_image_points(cache, images):
+    images_info = cache['images_info']
+
+    criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001)
+    
+    if not images_info:
+        print("'images_info' not found.")
+
+    if len(images_info.keys()) == 0:
+        print("No images in images_info. Please run detect_chessboard first.")
+
+    img_points = []
+    for key in tqdm(images):
+        camera = key.split("/")[0]
+
+        ret, corners = images_info[key]['findchessboardcorners_rgb']
+        if not ret:
+            continue
+        
+        image_gray = cv2.imread(images_info[key]['fullpath'], cv2.IMREAD_GRAYSCALE)
+        corners_refined = cv2.cornerSubPix(image_gray, corners, (5, 5), (-1, -1), criteria)
+
+        img_points.append(corners_refined)
+        width = image_gray.shape[1] # images_info[key]['width']
+        height = image_gray.shape[0] # ['height']
+
+    return img_points, width, height
+
+
+def find_matching_images(images_info, cam_1, cam_2):
+    images_info = cache['images_info']
+
+    matching_pairs = {}
+    for image in images_info.keys():
+        img_name = image.split("/")[1]
+        cam_1_img = f"{cam_1}/{img_name}"
+        cam_2_img = f"{cam_2}/{img_name}"
+        if images_info.__contains__(cam_1_img) \
+            and images_info.__contains__(cam_2_img):
+            points_found_1, _ = images_info[cam_1_img]['findchessboardcorners_rgb']
+            points_found_2, _ = images_info[cam_2_img]['findchessboardcorners_rgb']
+            if points_found_1 and points_found_2:
+                matching_pairs[img_name] = {
+                    "cam_1_img": cam_1_img,
+                    "cam_2_img": cam_2_img,
+                }
+            
+    return matching_pairs
+
+
+if __name__ == "__main__":
+    cache = diskcache.Cache('storage')
+
+    print("Cache keys:", get_all_keys(cache))
+
+    obj_points = get_obj_points()
+    intrinsics = cache['intrinsics']
+
+    cameras = list(intrinsics.keys())
+    for cam1_idx in range(len(cameras)):
+        for cam2_idx in range(cam1_idx + 1, len(cameras)):
+            print(f"Calibrating... {cameras[cam1_idx]}"
+                  f" vs {cameras[cam2_idx]}")
+
+            cam_1 = cameras[cam1_idx]
+            cam_2 = cameras[cam2_idx]
+
+            matching_pairs = find_matching_images(cache['images_info'], cam_1, cam_2)
+
+            print(f"Matching pairs: {len(matching_pairs)}")
+            if len(matching_pairs) > 0:
+                img_points_1, width, height = load_image_points(
+                    cache, images=[item['cam_1_img'] for item in matching_pairs.values()])
+                img_points_2, width, height = load_image_points(
+                    cache, images=[item['cam_2_img'] for item in matching_pairs.values()])
+
+                mtx_1 = intrinsics[cam_1]['mtx']
+                dist_1 = intrinsics[cam_1]['dist']
+                rvecs_1 = intrinsics[cam_1]['rvecs']
+                tvecs_1 = intrinsics[cam_1]['tvecs']
+
+                mtx_2 = intrinsics[cam_2]['mtx']
+                dist_2 = intrinsics[cam_2]['dist']
+                rvecs_2 = intrinsics[cam_2]['rvecs']
+                tvecs_2 = intrinsics[cam_2]['tvecs']
+
+                stereocalibration_criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 100, 1e-5)
+                stereocalibration_flags = 0 | cv2.CALIB_FIX_INTRINSIC
+                
+                # # STERO CALIBRATION
+                ret, mtx1, dist1, mtx2, dist2, R, T, E, F = cv2.stereoCalibrate(
+                    np.tile(obj_points, (len(img_points_1), 1, 1)),
+                    img_points_1, img_points_2,
+                    mtx_1, dist_1, mtx_2, dist_2, (width, height),
+                    criteria=stereocalibration_criteria, flags=stereocalibration_flags)
+
+                total_error = 0
+                for i in range(len(img_points_1)):
+                    imgpoints1_projected, _ = cv2.projectPoints(obj_points, rvecs_1[i], tvecs_1[i], mtx_1, dist_1)
+                    imgpoints2_projected, _ = cv2.projectPoints(obj_points, rvecs_2[i], tvecs_2[i], mtx_2, dist_2)
+
+                    imgpoints1_projected = imgpoints1_projected.reshape(-1, 2)
+                    imgpoints2_projected = imgpoints2_projected.reshape(-1, 2)
+
+                    error1 = cv2.norm(img_points_1[i], imgpoints1_projected, cv2.NORM_L2) / len(imgpoints1_projected)
+                    error2 = cv2.norm(img_points_2[i], imgpoints2_projected, cv2.NORM_L2) / len(imgpoints2_projected)
+                    print(f"Errors: {error1} {error2}")
+                    total_error += (error1 + error2) / 2
+
+                # Print the average projection error
+                print("Average projection error: ", total_error / len(img_points_1))
+
+
+
