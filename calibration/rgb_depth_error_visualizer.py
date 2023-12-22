ee7f7c01e1edec3a50ec4906c7282947b98169ca
sandstorm12 <sandstormeatwo@gmail.com>
2023-12-22 21:44:57 +0330

    Visualized the remapping of chessboard points across rgb and depth

--- calibration/rgb_depth_error_visualizer.py
+++ calibration/rgb_depth_error_visualizer.py
@@ -101,7 +101,7 @@ def calc_depth_rgb_match(cam_1, obj_points, cache):
         rgb_depth_pairs['image_points_rgb'],
         rgb_depth_pairs['image_points_infrared'],
         None, None, None, None,
-        (rgb_depth_pairs['width'], rgb_depth_pairs['height']),
+        (1920, 1080),
         criteria=STEREO_CALIBRATION_CRITERIA, flags=0)
     
     if not cache.__contains__('extrinsics'):
@@ -135,31 +135,11 @@ def calc_reprojection_error(cam_1, obj_points, cache):
     dist_2 = cache['depth_matching'][cam_1]['dist_r']
     R = cache['depth_matching'][cam_1]['rotation']
     T = cache['depth_matching'][cam_1]['transition']
-
-    RL, RR, PL, PR, Q, roiL, roiR = cv2.stereoRectify(
-        mtx_1, dist_1, mtx_2, dist_2, (640, 576), R, T)
-
-    map1, map2 = cv2.initUndistortRectifyMap(
-        mtx_1, dist_1, RR, PR,
-        (640, 576),
-        cv2.CV_32FC1)
-    
-    for idx in range(len(rgb_depth_pairs['image_paths_infrared'])):
-        img_rgb = cv2.imread(rgb_depth_pairs['image_paths_rgb'][idx], cv2.IMREAD_GRAYSCALE)
-        img_infrared = cv2.imread(rgb_depth_pairs['image_paths_infrared'][idx], cv2.IMREAD_GRAYSCALE)
-        img_infrared = cv2.equalizeHist(img_infrared)
-        undistorted_img = cv2.remap(img_rgb, map1, map2, interpolation=cv2.INTER_LINEAR)
-        # undistorted_img = cv2.resize(undistorted_img, (img_infrared.shape[1], img_infrared.shape[0]))
-        # undistorted_img = cv2.cvtColor(undistorted_img, cv2.COLOR_BGR2GRAY)
-        undistorted_img = ((img_infrared * 1. + undistorted_img * 1.) / 2).astype(np.uint8)
-        cv2.imshow("image", undistorted_img)
-        if cv2.waitKey(0) == ord('q'):
-            break
     
     total_error = 0
-    for i in range(len(rgb_depth_pairs['image_points_rgb'])):
+    for idx in range(len(rgb_depth_pairs['image_points_rgb'])):
         _, rvec_l, tvec_l = cv2.solvePnP(
-            obj_points, rgb_depth_pairs['image_points_rgb'][i], mtx_1, dist_1)
+            obj_points, rgb_depth_pairs['image_points_rgb'][idx], mtx_1, dist_1)
         rvec_r, tvec_r = cv2.composeRT(
             rvec_l, tvec_l, cv2.Rodrigues(R)[0], T)[:2]
 
@@ -167,13 +147,33 @@ def calc_reprojection_error(cam_1, obj_points, cache):
             obj_points, rvec_l, tvec_l, mtx_1, dist_1)
         imgpoints2_projected, _ = cv2.projectPoints(
             obj_points, rvec_r, tvec_r, mtx_2, dist_2)
+        
+        # img_rgb = cv2.imread(rgb_depth_pairs['image_paths_rgb'][idx], cv2.IMREAD_GRAYSCALE)
+        img_inf = cv2.imread(rgb_depth_pairs['image_paths_infrared'][idx], -1)
+        img_inf = np.clip(img_inf.astype(np.float32) * .8, 0, 255).astype('uint8')
+        img_inf = cv2.resize(img_inf, (1920, 1080))
+        for idx_point, point in enumerate(imgpoints2_projected):
+            x = int(point[0][0])
+            y = int(point[0][1])
+
+            cv2.circle(
+                img_inf, (x, y), 10, (0, 0, 0),
+                thickness=-1, lineType=8)
+
+            cv2.putText(
+                img_inf, str(idx_point), (x - 5, y + 5),
+                cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), thickness=2)
+            
+        cv2.imshow('Infrared', img_inf)
+        if cv2.waitKey(0) == ord('q'):
+            break
 
         error1 = cv2.norm(
-            rgb_depth_pairs['image_points_rgb'][i],
+            rgb_depth_pairs['image_points_rgb'][idx],
             imgpoints1_projected, cv2.NORM_L2) \
                 / len(imgpoints1_projected)
         error2 = cv2.norm(
-            rgb_depth_pairs['image_points_infrared'][i],
+            rgb_depth_pairs['image_points_infrared'][idx],
             imgpoints2_projected, cv2.NORM_L2) \
                 / len(imgpoints2_projected)
         print(f"Errors: cam1 {error1} cam2 {error2}")
