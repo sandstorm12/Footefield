6c87fe175fdc9583dd4426a9240030bd811e77a1
sandstorm12 <sandstormeatwo@gmail.com>
2023-12-23 17:35:43 +0330

    RGB to Infrared projection of chessboard points visualizer added

--- calibration/rgb_depth_error_visualizer.py
+++ calibration/rgb_depth_error_visualizer.py
@@ -45,7 +45,7 @@ def load_image_points(cache, images):
         image_gray = cv2.imread(
             images_info[key]['fullpath'], cv2.IMREAD_GRAYSCALE)
         corners_refined = cv2.cornerSubPix(
-            image_gray, corners, (5, 5), (-1, -1), criteria)
+            image_gray, corners, (11, 11), (-1, -1), criteria)
 
         img_points.append(corners_refined)
         width = image_gray.shape[1] # images_info[key]['width']
@@ -71,7 +71,8 @@ def find_rgb_depth_images(images_info, cam_1):
                 images_info[key]['findchessboardcorners_infrared']
             if points_found_rgb and points_found_infrared:
                 image_paths_rgb.append(images_info[key]['fullpath_rgb'])
-                image_paths_infrared.append(images_info[key]['fullpath_infrared'])
+                image_paths_infrared.append(
+                    images_info[key]['fullpath_infrared'])
                 image_points_rgb.append(points_rgb)
                 image_points_infrared.append(points_infrared)
                 width = images_info[key]['width']
@@ -87,36 +88,6 @@ def find_rgb_depth_images(images_info, cam_1):
     }
             
     return rgb_depth_pairs
-
-
-def calc_depth_rgb_match(cam_1, obj_points, cache):
-    print(f"Calibrating... {cam_1}")
-
-    rgb_depth_pairs = find_rgb_depth_images(cache['images_info'], cam_1)
-
-    print(f"Matching pairs: {len(rgb_depth_pairs['image_points_rgb'])}")
-
-    _, mtx_1, dist_1, mtx_2, dist_2, R, T, _, _ = cv2.stereoCalibrate(
-        np.tile(obj_points, (len(rgb_depth_pairs['image_points_rgb']), 1, 1)),
-        rgb_depth_pairs['image_points_rgb'],
-        rgb_depth_pairs['image_points_infrared'],
-        None, None, None, None,
-        (1920, 1080),
-        criteria=STEREO_CALIBRATION_CRITERIA, flags=0)
-    
-    if not cache.__contains__('extrinsics'):
-        cache['extrinsics'] = {}
-
-    depth_matching = cache.get('depth_matching', {})
-    depth_matching[cam_1] = {
-        'mtx_l': mtx_1,
-        'dist_l': dist_1,
-        'mtx_r': mtx_2,
-        'dist_r': dist_2,
-        'rotation': R,
-        'transition': T,
-    }
-    cache['depth_matching'] = depth_matching
 
 
 def calc_reprojection_error(cam_1, obj_points, cache):
@@ -136,22 +107,24 @@ def calc_reprojection_error(cam_1, obj_points, cache):
     R = cache['depth_matching'][cam_1]['rotation']
     T = cache['depth_matching'][cam_1]['transition']
     
-    total_error = 0
     for idx in range(len(rgb_depth_pairs['image_points_rgb'])):
         _, rvec_l, tvec_l = cv2.solvePnP(
-            obj_points, rgb_depth_pairs['image_points_rgb'][idx], mtx_1, dist_1)
+            obj_points, rgb_depth_pairs['image_points_rgb'][idx],
+            mtx_1, dist_1)
         rvec_r, tvec_r = cv2.composeRT(
             rvec_l, tvec_l, cv2.Rodrigues(R)[0], T)[:2]
 
-        imgpoints1_projected, _ = cv2.projectPoints(
-            obj_points, rvec_l, tvec_l, mtx_1, dist_1)
         imgpoints2_projected, _ = cv2.projectPoints(
             obj_points, rvec_r, tvec_r, mtx_2, dist_2)
         
-        # img_rgb = cv2.imread(rgb_depth_pairs['image_paths_rgb'][idx], cv2.IMREAD_GRAYSCALE)
         img_inf = cv2.imread(rgb_depth_pairs['image_paths_infrared'][idx], -1)
-        img_inf = np.clip(img_inf.astype(np.float32) * .8, 0, 255).astype('uint8')
-        img_inf = cv2.resize(img_inf, (1920, 1080))
+
+        # Remove magic number .8
+        img_inf = np.clip(
+            img_inf.astype(np.float32) * .8, 0, 255).astype('uint8')
+        img_inf = cv2.resize(
+            img_inf,
+            (data_loader.IMAGE_RGB_WIDTH, data_loader.IMAGE_RGB_HEIGHT))
         for idx_point, point in enumerate(imgpoints2_projected):
             x = int(point[0][0])
             y = int(point[0][1])
@@ -168,21 +141,6 @@ def calc_reprojection_error(cam_1, obj_points, cache):
         if cv2.waitKey(0) == ord('q'):
             break
 
-        error1 = cv2.norm(
-            rgb_depth_pairs['image_points_rgb'][idx],
-            imgpoints1_projected, cv2.NORM_L2) \
-                / len(imgpoints1_projected)
-        error2 = cv2.norm(
-            rgb_depth_pairs['image_points_infrared'][idx],
-            imgpoints2_projected, cv2.NORM_L2) \
-                / len(imgpoints2_projected)
-        print(f"Errors: cam1 {error1} cam2 {error2}")
-        total_error += (error1 + error2) / 2
-
-    # Print the average projection error
-    print("Average projection error: ",
-          total_error / len(rgb_depth_pairs['image_points_rgb']))
-
 
 if __name__ == "__main__":
     cache = diskcache.Cache('cache')
@@ -194,5 +152,4 @@ if __name__ == "__main__":
     for cam1_idx in range(len(cameras)):
         cam_1 = cameras[cam1_idx]
 
-        calc_depth_rgb_match(cam_1, obj_points, cache)
         calc_reprojection_error(cam_1, obj_points, cache)
