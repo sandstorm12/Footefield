d32062d26d7ef8b58aec8ab955cb1473cd9c2fb8
sandstorm12 <sandstormeatwo@gmail.com>
2023-12-26 21:11:22 +0330

    Reverse mapping for pose keypoints to the undistorted coordinates added

--- calibration/point_depth_finder.py
+++ calibration/point_depth_finder.py
@@ -6,14 +6,14 @@ import diskcache
 import numpy as np
 import matplotlib.pyplot as plt
 
-from tqdm import tqdm
 from utils import data_loader
 
 
 DISPARITY = -18
 
 
-def get_skeleton(image, inferencer, visualize=False):
+# Just for test
+def _get_skeleton(image, inferencer):
     result_generator = inferencer(image)
     
     detected_keypoints = []
@@ -21,43 +21,61 @@ def get_skeleton(image, inferencer, visualize=False):
         for predictions in result['predictions'][0]:
             keypoints = predictions['keypoints']
             detected_keypoints.append(keypoints)
-            for idx, point in enumerate(keypoints):
-
-                x = int(point[0])
-                y = int(point[1])
-
-                cv2.circle(
-                    image, (x, y), 10, (0),
-                    thickness=-1, lineType=8)
-
-                cv2.putText(
-                    image, str(idx), (x - 5, y + 5),
-                    cv2.FONT_HERSHEY_SIMPLEX, .5, (255), thickness=2)
-    
-    if visualize:
-        cv2.imshow('frame', image)
-        cv2.waitKey(0)
 
     return np.array(detected_keypoints)
 
 
-def points_to_depth(mmpose, image_rgb, image_inf, cache):
-    # points1 = keypoints.reshape((-1, 1, 2))
+def _map(x, y, mapx, mapy):
+    i = x
+    j = y
 
-    # print("keypoints", points1.shape)
+    # Calculate the distance of original point and out guessed point
+    delta_old = abs(mapx[y, x] - x) + abs(mapy[y, x] - y)
+    while True:
+        next_i = i
+        next_j = j
+        # Searching the 8 neighbour points for the smallest distance
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                # Make sure we don't go out of the image using a max-min
+                search_point_x = max(min(i+dx, mapx.shape[1] - 1), 0)
+                search_point_y = max(min(j+dy, mapx.shape[0] - 1), 0)
+                delta_x = abs(mapx[search_point_y, search_point_x] - x)
+                delta_y = abs(mapy[search_point_y, search_point_x] - y)
+                # If the distance of the new point was less than
+                # the distance of the older point, replace the point
+                if delta_old >= delta_x + delta_y:
+                    delta_old = delta_x + delta_y
+                    next_i = search_point_x
+                    next_j = search_point_y
 
-    cam_1 = 'azure_kinect3_4_calib_snap'
+        # If the newly found point is no better than the old point we stop
+        if next_i == i and next_j == j:
+            break
+        else:
+            i = next_i
+            j = next_j
 
+    return next_i, next_j
+
+
+def points_to_depth(keypoints, image_rgb, image_inf, camera, cache):
     if not cache.__contains__('depth_matching'):
         raise Exception('Depth matching not cached. '
                         'Run rgb_depth_calibration script.')
     
-    map1x = cache['depth_matching'][cam_1]['map_rgb_x']
-    map1y = cache['depth_matching'][cam_1]['map_rgb_y']
-    map2x = cache['depth_matching'][cam_1]['map_infrared_x']
-    map2y = cache['depth_matching'][cam_1]['map_infrared_y']
+    map1x = cache['depth_matching'][camera]['map_rgb_x']
+    map1y = cache['depth_matching'][camera]['map_rgb_y']
+    map2x = cache['depth_matching'][camera]['map_infrared_x']
+    map2y = cache['depth_matching'][camera]['map_infrared_y']
     
-    image_rgb = cv2.remap(image_rgb, map1x, map1y, cv2.INTER_LANCZOS4)
+    image_rgb = cv2.remap(image_rgb, map1x, map1y, cv2.INTER_LINEAR)
+    
+    for i in range(keypoints.shape[0]):
+        for j in range(keypoints.shape[1]):
+            x, y = keypoints[i, j]
+            keypoints[i, j] = _map(int(x), int(y), map1x, map1y)
+
 
     # Remove magic number .8
     image_inf = cv2.resize(
@@ -70,7 +88,7 @@ def points_to_depth(mmpose, image_rgb, image_inf, cache):
     image_inf = np.clip(
             image_inf.astype(np.float32) * .1, 0, 255).astype('uint8')
     
-    keypoints = get_skeleton(image_rgb, mmpose, visualize=False)
+    # keypoints = _get_skeleton(image_rgb, mmpose, visualize=False)
     
     for person_keypoints in keypoints:
         for idx, point in enumerate(person_keypoints):
@@ -78,40 +96,26 @@ def points_to_depth(mmpose, image_rgb, image_inf, cache):
             y = int(point[1])
 
             cv2.circle(
+                image_rgb, (x, y), 10, (0, 0, 0),
+                thickness=-1, lineType=8)
+            cv2.circle(
                 image_inf, (x, y), 10, (0, 0, 0),
                 thickness=-1, lineType=8)
 
+            cv2.putText(
+                image_rgb, str(idx), (x - 5, y + 5),
+                cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), thickness=2)
             cv2.putText(
                 image_inf, str(idx), (x - 5, y + 5),
                 cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), thickness=2)
     
     image_rgb = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
-    img_cmb = (image_rgb * .5 + image_inf * .5).astype(np.uint8)
-
-    # cv2.imshow("CMB", image_inf)
-    # cv2.waitKey(0)
-
-    # cv2.circle(
-    #     image_1, (x, y), 10, (0, 0, 0),
-    #     thickness=-1, lineType=8)
-    
-    # # image_2 = np.clip(image_2.astype(np.float32) * 2., 0, 255).astype('uint8')
-    # cv2.circle(
-    #     image_2, (int(map2x[x, y]), int(map2y[x, y])), 10, (100, 100, 100),
-    #     thickness=-1, lineType=8)
-    
-    # # print()
 
     f, axarr = plt.subplots(1, 2)
     axarr[0].imshow(image_rgb)
     axarr[1].imshow(image_inf)
 
-    # plt.imshow(image_inf)
-    # plt.imshow(image_rgb)
     plt.show()
-
-    # # Print the result
-    # print(f"The corresponding point in the right image is ({point_right})")
 
 
 # Just for test
@@ -121,15 +125,17 @@ if __name__ == "__main__":
     from mmpose.apis import MMPoseInferencer
     mmpose = MMPoseInferencer('human')
 
+    camera = 'azure_kinect3_4_calib_snap'
+
     img_rgb_path = '/home/hamid/Documents/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_4/color/color00000.jpg'
     img_dpt_path = '/home/hamid/Documents/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_4/depth/depth00000.png'
     img_rgb = cv2.imread(img_rgb_path)
     img_dpt = cv2.imread(img_dpt_path, -1)
     img_dpt = cv2.resize(img_dpt, (1920, 1080))
 
-    # keypoints = get_skeleton(img_rgb, mmpose, visualize=False)
+    keypoints = _get_skeleton(img_rgb, mmpose)
 
-    points_to_depth(mmpose, img_rgb, img_dpt, cache)
+    points_to_depth(keypoints, img_rgb, img_dpt, camera, cache)
 
     # img_dpt = np.clip(img_dpt.astype(np.float32) * 2, 0, 255).astype('uint8')
     # cv2.imshow("Depth", img_dpt)
