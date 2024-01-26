bd0df648382cea506bedd7c6cf7502953de10e67
sandstorm12 <sandstormeatwo@gmail.com>
2024-01-27 01:24:07 +0330

    New skeleton normalization and correction added to skeleton_2_numpypkl

--- pose_estimation/skeleton_2_numpypkl.py
+++ pose_estimation/skeleton_2_numpypkl.py
@@ -10,24 +10,24 @@ from numpy.linalg import norm
 OUTPUT_DIR = './skeleton_numpypkl'
 
 
-# Define a function to calculate the magnitude of a vector
 def magnitude (v):
     return math.sqrt (sum (v [i] ** 2 for i in range (len (v))))
 
-# Define a function to calculate the dot product of two vectors
+
 def dot_product (v1, v2):
     return sum (v1 [i] * v2 [i] for i in range (len (v1)))
 
-# Define a function to calculate the angle between two vectors
+
 def angle (v1, v2):
     cos_theta = dot_product (v1, v2) / (magnitude (v1) * magnitude (v2))
+
     return math.degrees (math.acos (cos_theta))
 
-# Define a function to calculate the vector difference of two points
+
 def vector_difference (p1, p2):
     return tuple (p2 [i] - p1 [i] for i in range (len (p1)))
 
-# Define a function to calculate the angles between a line and the x, y, and z axis
+
 def line_angles (p1, p2):
     # Define the x, y, and z axis as unit vectors
     x_axis = (1, 0, 0)
@@ -82,7 +82,7 @@ def rotate_z(points, angle):
     rotation_matrix = [[cos, -sin, 0], [sin, cos, 0], [0, 0, 1]]
     return [[sum([rotation_matrix[i][j] * point[j] for j in range(3)]) for i in range(3)] for point in points]
 
-
+# TODO: Needs heavy refactor
 def skeleton_2_numpypkl(output_dir, cache_process):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -92,8 +92,6 @@ def skeleton_2_numpypkl(output_dir, cache_process):
             skeleton = np.array([keypoints[0]
                                  for keypoints in cache_process[key]],
                                 dtype=float)
-            
-            print(skeleton.shape)
             
             if skeleton.shape[0] != 200:
                 continue
@@ -111,40 +109,39 @@ def skeleton_2_numpypkl(output_dir, cache_process):
 
                 x_angle, y_angle, z_angle = line_angles(skeleton[i][5], skeleton[i][6])
 
-                print("Before", x_angle, y_angle, z_angle)
-
                 while True:
                     x_angle, y_angle, z_angle = line_angles(skeleton[i][5], skeleton[i][6])
-                    if abs(z_angle - 90) < 1:
+                    if abs(z_angle - 80) < 1:
                         break
                     skeleton[i] = np.array(rotate_y(skeleton[i], 1))  # facing angle
 
                 x_angle, y_angle, z_angle = line_angles(skeleton[i][5], skeleton[i][6])
 
-                print("After", x_angle, y_angle, z_angle)
+                skeleton[i][20] = (skeleton[i][20] + skeleton[i][22]) / 2
+                skeleton[i][21] = (skeleton[i][21] + skeleton[i][23]) / 2
 
-                scale = np.sqrt(np.sum((skeleton[i][8] - skeleton[i][10]) ** 2))
-                print(scale)
+                upper_scale = abs(skeleton[i][18][1] - skeleton[i][19][1])
+                lower_scale = abs(skeleton[i][12][1] - skeleton[i][16][1])
+                transform_scale = 3*lower_scale/(4*upper_scale)
 
-                # skeleton[i][21] = (skeleton[i][21] + skeleton[i][23]) / 2
-                # skeleton[i][20] = (skeleton[i][20] + skeleton[i][22]) / 2
-
-                # skeleton[i][5][1] = skeleton[i][5][1] - .4 * scale
-                # skeleton[i][6][1] = skeleton[i][6][1] - .4 * scale
-                # skeleton[i][7][1] = skeleton[i][7][1] - .4 * scale
-                # skeleton[i][8][1] = skeleton[i][8][1] - .4 * scale
-                # skeleton[i][9][1] = skeleton[i][9][1] - .4 * scale
-                # skeleton[i][10][1] = skeleton[i][10][1] - .4 * scale
+                for j in range(len(skeleton[i])):
+                    if skeleton[i][j][1] > 0:
+                        skeleton[i][j][1] = skeleton[i][j][1] * transform_scale * .9
                 
-                # skeleton[i][11][0] = skeleton[i][11][0] - .15 * scale
-                # skeleton[i][11][1] = skeleton[i][11][1] + .4 * scale
+                COEFF_1 = 1.5
+                skeleton[i][5][0] = skeleton[i][5][0] * COEFF_1
+                skeleton[i][6][0] = skeleton[i][6][0] * COEFF_1
+                skeleton[i][7][0] = skeleton[i][7][0] * COEFF_1
+                skeleton[i][8][0] = skeleton[i][8][0] * COEFF_1
+                # skeleton[i][9][0] = skeleton[i][9][0] * COEFF_1
+                # skeleton[i][10][0] = skeleton[i][10][0] * COEFF_1
 
-                # skeleton[i][12][0] = skeleton[i][12][0] + .15 * scale
-                # skeleton[i][12][1] = skeleton[i][12][1] + .4 * scale
+                COEFF_2 = 1.5
+                skeleton[i][13][0] = skeleton[i][13][0] * COEFF_2
+                skeleton[i][14][0] = skeleton[i][14][0] * COEFF_2
+                skeleton[i][15][0] = skeleton[i][15][0] * COEFF_2
+                skeleton[i][16][0] = skeleton[i][16][0] * COEFF_2
 
-                # skeleton[i][18][1] = skeleton[i][18][1] - .4 * scale
-                # skeleton[i][19][1] = skeleton[i][19][1] + .4 * scale
-                
             output_path = os.path.join(output_dir, f'{key}.npy')
             with open(output_path, 'wb') as handle:
                 np.save(handle, skeleton)
