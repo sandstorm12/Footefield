b32f0983f36991fe0fc1c747a05c976550f4d8b2
sandstorm12 <sandstormeatwo@gmail.com>
2024-02-21 04:07:22 +0330

    A single subject point cloud visualizer video implemented

---
+++ smpl/visualizer_pointcloud_video_subject.py
@@ -0,0 +1,249 @@
+import sys
+sys.path.append('../')
+
+import os
+import time
+import copy
+import diskcache
+import numpy as np
+import open3d as o3d
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+
+from utils import data_loader
+
+
+def load_pointcloud(path, cam, idx, cache):
+    path_depth = os.path.join(path, 'depth/depth{:05d}.png'.format(idx))
+    depth = o3d.io.read_image(path_depth)
+
+    intrinsics, extrinsics = get_parameters(cam, cache)
+
+    pc = o3d.geometry.PointCloud.create_from_depth_image(depth, intrinsics, extrinsics)
+
+    return pc
+
+
+# TODO: Shorten
+def get_parameters(cam, cache):
+    if cam == cam24:
+        mtx = cache['extrinsics'][cam24 + 'infrared']['mtx_l']
+    elif cam == cam15:
+        mtx = cache['extrinsics'][cam24 + 'infrared']['mtx_r']
+    elif cam == cam14:
+        mtx = cache['extrinsics'][cam15 + 'infrared']['mtx_r']
+    elif cam == cam34:
+        mtx = cache['extrinsics'][cam14 + 'infrared']['mtx_r']
+    elif cam == cam35:
+        mtx = cache['extrinsics'][cam34 + 'infrared']['mtx_r']
+    
+    R = cache['extrinsics'][cam24 + 'infrared']['rotation']
+    T = cache['extrinsics'][cam24 + 'infrared']['transition']
+    R2 = cache['extrinsics'][cam15 + 'infrared']['rotation']
+    T2 = cache['extrinsics'][cam15 + 'infrared']['transition']
+    R3 = cache['extrinsics'][cam14 + 'infrared']['rotation']
+    T3 = cache['extrinsics'][cam14 + 'infrared']['transition']
+    R4 = cache['extrinsics'][cam34 + 'infrared']['rotation']
+    T4 = cache['extrinsics'][cam34 + 'infrared']['transition']
+
+    intrinsics = o3d.camera.PinholeCameraIntrinsic(
+        data_loader.IMAGE_INFRARED_WIDTH,
+        data_loader.IMAGE_INFRARED_HEIGHT,
+        mtx[0, 0], mtx[1, 1], mtx[0, 2], mtx[1, 2])
+    
+    extrinsics = np.identity(4)
+    if cam == cam24:
+        r = np.array([[1, 0, 0],
+                     [0, 1, 0],
+                     [0, 0, 1]])
+        t = np.array([0, 0, 0])
+        extrinsics[:3, :3] = r
+        extrinsics[:3, 3] = t.reshape(3)
+    elif cam == cam15:
+        extrinsics[:3, :3] = R
+        extrinsics[:3, 3] = T.reshape(3) / 1000
+    elif cam == cam14:
+        R2_com = np.dot(R2, R)
+        T2_com = (np.dot(R2, T).reshape(3, 1) + T2).reshape(3,)
+        extrinsics[:3, :3] = R2_com
+        extrinsics[:3, 3] = T2_com / 1000
+    elif cam == cam34:
+        R2_com = np.dot(R2, R)
+        T2_com = (np.dot(R2, T).reshape(3, 1) + T2).reshape(3,)
+        R3_com = np.dot(R3, R2_com)
+        T3_com = (np.dot(R3, T2_com).reshape(3, 1) + T3).reshape(3,)
+        extrinsics[:3, :3] = R3_com
+        extrinsics[:3, 3] = T3_com / 1000
+    elif cam == cam35:
+        R2_com = np.dot(R2, R)
+        T2_com = (np.dot(R2, T).reshape(3, 1) + T2).reshape(3,)
+        R3_com = np.dot(R3, R2_com)
+        T3_com = (np.dot(R3, T2_com).reshape(3, 1) + T3).reshape(3,)
+        R4_com = np.dot(R4, R3_com)
+        T4_com = (np.dot(R4, T3_com).reshape(3, 1) + T4).reshape(3,)
+        extrinsics[:3, :3] = R4_com
+        extrinsics[:3, 3] = T4_com / 1000
+
+    return intrinsics, extrinsics
+
+
+
+def remove_outliers(pointcloud):
+    _, ind = pointcloud.remove_statistical_outlier(
+        nb_neighbors=20,
+        std_ratio=.1)
+    pointcloud = pointcloud.select_by_index(ind)
+    
+    return pointcloud
+
+
+def preprocess(pointcloud):
+    pointcloud = remove_outliers(pointcloud)
+
+    pointcloud = pointcloud.voxel_down_sample(voxel_size=0.01)
+
+    return pointcloud
+
+
+def show(pointcloud):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(visible=True)
+    vis.get_render_option().background_color = [.5, .5, .5]
+    vis.add_geometry(pointcloud)
+    vis.run()
+
+
+def get_subject(experiment, subject, idx, cache):
+    start_pts = np.array([[0, 0, 0], [2e3, 2e3, 2e3]])
+    
+    # Cam24
+    path = data_loader.EXPERIMENTS[experiment][cam24]
+    pc24 = load_pointcloud(path, cam24, idx, cache)
+    pc24 = preprocess(pc24)
+    pc_np = np.asarray(pc24.points)
+    kmeans = KMeans(n_clusters=2, random_state=47, init=start_pts, n_init=1).fit(pc_np)
+    # TODO: Explain what (subject + 1 % 2) is
+    pc24.points = o3d.utility.Vector3dVector(pc_np[kmeans.labels_ == (subject + 1) % 2])
+    
+    # Cam15
+    path = data_loader.EXPERIMENTS[experiment][cam15]
+    pc15 = load_pointcloud(path, cam15, idx, cache)
+    pc15 = preprocess(pc15)
+    pc_np = np.asarray(pc15.points)
+    kmeans = KMeans(n_clusters=2, random_state=47, init=start_pts, n_init=1).fit(pc_np)
+    pc15.points = o3d.utility.Vector3dVector(pc_np[kmeans.labels_ == (subject) % 2])
+
+    # Cam14
+    path = data_loader.EXPERIMENTS[experiment][cam14]
+    pc14 = load_pointcloud(path, cam14, idx, cache)
+    pc14 = preprocess(pc14)
+
+    # Cam34
+    path = data_loader.EXPERIMENTS[experiment][cam34]
+    pc34 = load_pointcloud(path, cam34, idx, cache)
+    pc34 = preprocess(pc34)
+    pc_np = np.asarray(pc34.points)
+    kmeans = KMeans(n_clusters=2, random_state=47, init=start_pts, n_init=1).fit(pc_np)
+    pc34.points = o3d.utility.Vector3dVector(pc_np[kmeans.labels_ == (subject + 1) % 2])
+
+    # Cam35
+    path = data_loader.EXPERIMENTS[experiment][cam35]
+    pc35 = load_pointcloud(path, cam35, idx, cache)
+    pc35 = preprocess(pc35)
+    pc_np = np.asarray(pc35.points)
+    kmeans = KMeans(n_clusters=2, random_state=47, init=start_pts, n_init=1).fit(pc_np)
+    pc35.points = o3d.utility.Vector3dVector(pc_np[kmeans.labels_ == (subject + 1) % 2])
+
+    return {
+        cam24: pc24,
+        cam15: pc15,
+        cam14: pc14,
+        cam34: pc34,
+        cam35: pc35,
+    }
+
+
+def rotation_matrix_from_euler_angles(roll, pitch, yaw):
+    """
+        Creates a 3x3 rotation matrix from euler angles (roll, pitch, yaw).
+
+        Args:
+        roll (float): Roll angle in radians.
+        pitch (float): Pitch angle in radians.
+        yaw (float): Yaw angle in radians.
+
+        Returns:
+        rotation_matrix (np.ndarray): 3x3 rotation matrix.
+    """
+
+    rotation_matrix = np.identity(3)
+
+    # Roll
+    c_roll = np.cos(roll)
+    s_roll = np.sin(roll)
+
+    rotation_matrix = np.dot(rotation_matrix, np.array(
+        [[1, 0, 0],
+        [0, c_roll, -s_roll],
+        [0, s_roll, c_roll]]))
+
+    # Pitch
+    c_pitch = np.cos(pitch)
+    s_pitch = np.sin(pitch)
+
+    rotation_matrix = np.dot(rotation_matrix, np.array(
+        [[c_pitch, 0, s_pitch],
+        [0, 1, 0],
+        [-s_pitch, 0, c_pitch]]))
+
+    # Yaw
+    c_yaw = np.cos(yaw)
+    s_yaw = np.sin(yaw)
+
+    rotation_matrix = np.dot(rotation_matrix, np.array(
+        [[c_yaw, s_yaw, 0],
+        [-s_yaw, c_yaw, 0],
+        [0, 0, 1]]))
+
+    return rotation_matrix
+
+
+cam24 = 'azure_kinect2_4_calib_snap'
+cam15 = 'azure_kinect1_5_calib_snap'
+cam14 = 'azure_kinect1_4_calib_snap'
+cam34 = 'azure_kinect3_4_calib_snap'
+cam35 = 'azure_kinect3_5_calib_snap'
+if __name__ == "__main__":
+    cache = diskcache.Cache('../calibration/cache')
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+
+    geometry = o3d.geometry.PointCloud()
+    geometry_mesh = o3d.geometry.TriangleMesh()
+
+    for i in range(1000):
+        subject_0 = get_subject('a1', 1, i, cache)
+
+        transition = [0.09, 0.10999999999999999, 0.02]
+        angle = [0, 0, 0]
+        rm = rotation_matrix_from_euler_angles(np.deg2rad(angle[0]),
+                                            np.deg2rad(angle[1]),
+                                            np.deg2rad(angle[2]))
+        subject_0[cam35].rotate(rm)
+        subject_0[cam35].translate(transition)
+
+        pc_s0 = subject_0[cam34] + subject_0[cam35]
+
+        geometry.points = pc_s0.points
+        if i == 0:
+            vis.add_geometry(geometry)
+        else:
+            vis.update_geometry(geometry)
+            
+        vis.poll_events()
+        vis.update_renderer()
+
+        print(f"Update {i}: {time.time()}")
+        # time.sleep(.05)
