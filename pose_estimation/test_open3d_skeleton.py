c236676e12b5cbe00fd0fb79c9eff1c1a4d08acf
sandstorm12 <sandstormeatwo@gmail.com>
2024-02-24 01:00:17 +0330

    An unfinished version of 3d keypoints visualizer on pointcloud added

--- pose_estimation/test_open3d_skeleton.py
+++ pose_estimation/test_open3d_skeleton.py
@@ -2,12 +2,16 @@ import sys
 sys.path.append('../')
 
 import os
+import cv2
 import math
 import glob
 import time
 import diskcache
 import numpy as np
 import open3d as o3d
+
+from utils import data_loader
+from calibration import rgb_depth_map
 
 
 HALPE_LINES = np.array(
@@ -17,6 +21,8 @@ HALPE_LINES = np.array(
      (21, 25), (23, 25), (22, 24), (15, 24), (16, 25)])
 
 def visualize_poses(poses):
+    cache = diskcache.Cache('../calibration/cache')
+
     vis = o3d.visualization.VisualizerWithKeyCallback()
     vis.create_window()
 
@@ -25,8 +31,44 @@ def visualize_poses(poses):
     geometry = o3d.geometry.PointCloud()
     lines = o3d.geometry.LineSet()
 
-    for i in range(len(poses)):
-        keypoints = poses[i].reshape(-1, 3)
+    geometry_image = o3d.geometry.PointCloud()
+
+    for idx in range(len(poses)):
+        camera = 'azure_kinect3_5_calib_snap'
+
+        img_depth_path = f'/home/hamid/Documents/phd/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_5/depth/depth0000{idx}.png'
+        img_color_path = f'/home/hamid/Documents/phd/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_5/color/color0000{idx}.jpg'
+
+        img_depth = cv2.imread(img_depth_path, -1)
+        img_color = cv2.imread(img_color_path)
+        img_color = data_loader.downsample_keep_aspect_ratio(
+            img_color,
+            (
+                data_loader.IMAGE_INFRARED_WIDTH,
+                data_loader.IMAGE_INFRARED_HEIGHT
+            )
+        )
+
+        img_color = rgb_depth_map.align_image_rgb(img_color, camera, cache)
+        img_depth = rgb_depth_map.align_image_depth(img_depth, camera, cache)
+
+        print(img_depth.shape)
+
+        point_cloud = []
+        colors = []
+        for i in range(img_depth.shape[1]):
+            for j in range(img_depth.shape[0]):
+                if img_depth[j, i] != 0:
+                    point_cloud.append((i, j, img_depth[j, i]))
+                    colors.append((img_color[j, i, 0] / 255.,
+                                    img_color[j, i, 1] / 255.,
+                                    img_color[j, i, 2] / 255.))
+
+        pcd_image = o3d.geometry.PointCloud()
+        pcd_image.points = o3d.utility.Vector3dVector(point_cloud)
+        pcd_image.colors = o3d.utility.Vector3dVector(colors)
+
+        keypoints = poses[idx].reshape(-1, 3)
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(keypoints)
         pcd.paint_uniform_color([1, 0, 0]) # red points
@@ -37,20 +79,25 @@ def visualize_poses(poses):
         lines.lines = o3d.utility.Vector2iVector(connections)
 
         geometry.points = pcd.points
-        if i == 0:
+        geometry_image.points = pcd_image.points
+        if idx == 0:
             vis.add_geometry(geometry)
+            vis.add_geometry(geometry_image)
             vis.add_geometry(lines)
             vis.add_geometry(axis)
         else:
             vis.update_geometry(geometry)
+            vis.update_geometry(geometry_image)
             vis.update_geometry(lines)
             vis.update_geometry(axis)
         
-        vis.poll_events()
-        vis.update_renderer()
+        for delay in range(1000):
+            vis.poll_events()
+            vis.update_renderer()
+            time.sleep(.01)
 
-        print(f"Update {i}: {time.time()}")
-        time.sleep(.2)
+        print(f"Update {idx}: {time.time()}")
+            
 
 
 
@@ -60,12 +107,12 @@ if __name__ == "__main__":
 
     cache_process = cache.get('process', {})
 
-    for key in cache_process.keys():
-        if "skeleton_3D_smooth" in key:
-            print(f"Visualizing {key}")
+    for file in os.listdir('keypoints_3d'):
+        file_path = os.path.join('keypoints_3d', file)
+        print(f"Visualizing {file_path}")
 
-            poses = cache_process[key]
-            poses = np.array(poses)
+        import pickle
+        with open(file_path, 'rb') as handle:
+            poses = np.array(pickle.load(handle))
 
-            if poses.shape[0] == 200:
-                visualize_poses(poses)
+        visualize_poses(poses)
