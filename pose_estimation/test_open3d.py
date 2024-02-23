fad3997a439c1cf6b1187356e217ac4547f2613c
sandstorm12 <sandstormeatwo@gmail.com>
2024-02-24 00:58:05 +0330

    Added undistortion to aligned rgbd open3d visualizer for a more accurate representation

--- pose_estimation/test_open3d.py
+++ pose_estimation/test_open3d.py
@@ -30,22 +30,24 @@ img_color = data_loader.downsample_keep_aspect_ratio(
 img_color = rgb_depth_map.align_image_rgb(img_color, camera, cache)
 img_depth = rgb_depth_map.align_image_depth(img_depth, camera, cache)
 
-print(img_depth.shape)
+color0 = o3d.geometry.Image((img_color).astype(np.uint8))
+depth0 = o3d.geometry.Image(img_depth)
 
-point_cloud = []
-colors = []
-for i in range(img_depth.shape[1]):
-    for j in range(img_depth.shape[0]):
-        if img_depth[j, i] != 0:
-            point_cloud.append((i / 640., 1 - (j / 576.), 1 - (img_depth[j, i] / 5000.)))
-            colors.append((img_color[j, i, 0] / 255.,
-                            img_color[j, i, 1] / 255.,
-                            img_color[j, i, 2] / 255.))
+mtx0 = cache['extrinsics'][camera + 'infrared']['mtx_l']
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(point_cloud)
-pcd.colors = o3d.utility.Vector3dVector(colors)
-# o3d.visualization.draw_geometries([pcd])
+rgbd0 = o3d.geometry.RGBDImage.create_from_color_and_depth(color0, depth0)
+
+intrinsic0 = o3d.camera.PinholeCameraIntrinsic(640, 576, mtx0[0, 0], mtx0[1, 1], mtx0[0, 2], mtx0[1, 2])
+
+extrinsic0 = np.identity(4) # Extrinsic matrix
+r = np.array([[1, 0, 0], # Rotation matrix
+            [0, 1, 0],
+            [0, 0, 1]])
+t = np.array([0, 0, 0]) # Translation vector
+extrinsic0[:3, :3] = r
+extrinsic0[:3, 3] = t.reshape(3)
+
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd0, intrinsic0, extrinsic0)
 
 vis = o3d.visualization.Visualizer()
 vis.create_window()
