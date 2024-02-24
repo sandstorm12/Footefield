35dd9c5163e7eae99413f6bf73e092d2b2d50e81
sandstorm12 <sandstormeatwo@gmail.com>
2024-02-25 00:14:07 +0330

    Point cloud and 3d skeleton keypoints alignment script added

--- pose_estimation/test_open3d_skeleton.py
+++ pose_estimation/test_open3d_skeleton.py
@@ -36,8 +36,8 @@ def visualize_poses(poses):
     for idx in range(len(poses)):
         camera = 'azure_kinect3_5_calib_snap'
 
-        img_depth_path = f'/home/hamid/Documents/phd/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_5/depth/depth0000{idx}.png'
-        img_color_path = f'/home/hamid/Documents/phd/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_5/color/color0000{idx}.jpg'
+        img_depth_path = '/home/hamid/Documents/phd/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_5/depth/depth{:05d}.png'.format(idx)
+        img_color_path = '/home/hamid/Documents/phd/footefield/data/AzureKinectRecord_0729/a1/azure_kinect3_5/color/color{:05d}.jpg'.format(idx)
 
         img_depth = cv2.imread(img_depth_path, -1)
         img_color = cv2.imread(img_color_path)
@@ -50,24 +50,29 @@ def visualize_poses(poses):
         )
 
         img_color = rgb_depth_map.align_image_rgb(img_color, camera, cache)
-        img_depth = rgb_depth_map.align_image_depth(img_depth, camera, cache)
+        color0 = o3d.geometry.Image((img_color).astype(np.uint8))
+        depth0 = o3d.geometry.Image(img_depth)
 
-        print(img_depth.shape)
+        mtx0 = cache['extrinsics']['azure_kinect3_4_calib_snap' + 'infrared']['mtx_r']
 
-        point_cloud = []
-        colors = []
-        for i in range(img_depth.shape[1]):
-            for j in range(img_depth.shape[0]):
-                if img_depth[j, i] != 0:
-                    point_cloud.append((i, j, img_depth[j, i]))
-                    colors.append((img_color[j, i, 0] / 255.,
-                                    img_color[j, i, 1] / 255.,
-                                    img_color[j, i, 2] / 255.))
+        rgbd0 = o3d.geometry.RGBDImage.create_from_color_and_depth(color0, depth0, convert_rgb_to_intensity=False)
 
-        pcd_image = o3d.geometry.PointCloud()
-        pcd_image.points = o3d.utility.Vector3dVector(point_cloud)
-        pcd_image.colors = o3d.utility.Vector3dVector(colors)
+        intrinsic0 = o3d.camera.PinholeCameraIntrinsic(640, 576, mtx0[0, 0], mtx0[1, 1], mtx0[0, 2], mtx0[1, 2])
 
+        extrinsic0 = np.identity(4) # Extrinsic matrix
+        r = np.array([[1, 0, 0], # Rotation matrix
+                    [0, 1, 0],
+                    [0, 0, 1]])
+        t = np.array([0, 0, 0]) # Translation vector
+        extrinsic0[:3, :3] = r
+        extrinsic0[:3, 3] = t.reshape(3)
+
+        pcd_image = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd0, intrinsic0, extrinsic0)
+        pcd_image.paint_uniform_color([0, 1, 0]) # red points
+
+        poses[idx][:,:,2] /= 1000.
+        poses[idx][:,:,0] = (poses[idx][:,:,0] - mtx0[0, 2]) * poses[idx][:,:,2] / mtx0[0, 0]
+        poses[idx][:,:,1] = (poses[idx][:,:,1] - mtx0[1, 2]) * poses[idx][:,:,2] / mtx0[1, 1]
         keypoints = poses[idx].reshape(-1, 3)
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(keypoints)
@@ -79,28 +84,27 @@ def visualize_poses(poses):
         lines.lines = o3d.utility.Vector2iVector(connections)
 
         geometry.points = pcd.points
+        geometry.colors = pcd.colors
         geometry_image.points = pcd_image.points
+        geometry_image.colors = pcd_image.colors
         if idx == 0:
             vis.add_geometry(geometry)
-            vis.add_geometry(geometry_image)
             vis.add_geometry(lines)
-            vis.add_geometry(axis)
+            # vis.add_geometry(axis)
+            vis.add_geometry(geometry_image)
         else:
             vis.update_geometry(geometry)
-            vis.update_geometry(geometry_image)
             vis.update_geometry(lines)
-            vis.update_geometry(axis)
-        
-        for delay in range(1000):
+            # vis.update_geometry(axis)
+            vis.update_geometry(geometry_image)
+            
+        for delay in range(20):
             vis.poll_events()
             vis.update_renderer()
             time.sleep(.01)
 
         print(f"Update {idx}: {time.time()}")
             
-
-
-
 
 if __name__ == "__main__":
     cache = diskcache.Cache('../calibration/cache')
