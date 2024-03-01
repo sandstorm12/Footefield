1ceb3ea588543b6cfe38aadd3c502370795e1403
sandstorm12 <sandstormeatwo@gmail.com>
2024-03-01 20:54:08 +0330

    1. Added the calibration parameter as a variable in the optimization script. 2. Removed the distortion parameter from being optimized because we don't have distortion in point cloud visualization in Open3D

--- pose_estimation/skeleton_bundle_adjustment.py
+++ pose_estimation/skeleton_bundle_adjustment.py
@@ -13,6 +13,7 @@ from scipy.optimize import least_squares
 
 DIR_INPUT = './keypoints_3d'
 DIR_STORE = './keypoints_3d_ba'
+PARAM_CALIB_SIZE = 16
 
 
 def read_ba_data(path):
@@ -36,7 +37,7 @@ def project(points, params):
     camera_matrix[:, 1, 1] = params[:, 13]
     camera_matrix[:, 0, 2] = params[:, 14]
     camera_matrix[:, 1, 2] = params[:, 15]
-    dist_coeffs = params[:, 16:]
+    # dist_coeffs = params[:, 16:]
     rotation = params[:, :9].reshape(-1, 3, 3)
     translation = params[:, 9:12]
 
@@ -45,7 +46,7 @@ def project(points, params):
         points_proj.append(
             cv2.projectPoints(
                 point, rotation[idx], translation[idx],
-                camera_matrix[idx], dist_coeffs[idx])[0]
+                camera_matrix[idx], None)[0]
         )
 
     points_proj = np.asarray(points_proj).squeeze()
@@ -55,8 +56,9 @@ def project(points, params):
 
 def fun(params, n_cameras, n_points, camera_indices, point_indices,
         points_2d):
-    camera_params = params[:n_cameras * 21].reshape((n_cameras, 21))
-    points_3d = params[n_cameras * 21:].reshape((n_points, 3))
+    camera_params = params[:n_cameras * PARAM_CALIB_SIZE].reshape(
+        (n_cameras, PARAM_CALIB_SIZE))
+    points_3d = params[n_cameras * PARAM_CALIB_SIZE:].reshape((n_points, 3))
     points_proj = project(
         points_3d[point_indices], camera_params[camera_indices])
 
@@ -66,23 +68,23 @@ def fun(params, n_cameras, n_points, camera_indices, point_indices,
 def bundle_adjustment_sparsity(n_cameras, n_points, camera_indices,
                                point_indices):
     m = camera_indices.size * 2
-    n = n_cameras * 21 + n_points * 3
+    n = n_cameras * PARAM_CALIB_SIZE + n_points * 3
     A = lil_matrix((m, n), dtype=int)
 
     i = np.arange(camera_indices.size)
-    for s in range(21):
-        A[2 * i, camera_indices * 21 + s] = 1
-        A[2 * i + 1, camera_indices * 21 + s] = 1
+    for s in range(PARAM_CALIB_SIZE):
+        A[2 * i, camera_indices * PARAM_CALIB_SIZE + s] = 1
+        A[2 * i + 1, camera_indices * PARAM_CALIB_SIZE + s] = 1
 
     for s in range(3):
-        A[2 * i, n_cameras * 21 + point_indices * 3 + s] = 1
-        A[2 * i + 1, n_cameras * 21 + point_indices * 3 + s] = 1
+        A[2 * i, n_cameras * PARAM_CALIB_SIZE + point_indices * 3 + s] = 1
+        A[2 * i + 1, n_cameras * PARAM_CALIB_SIZE + point_indices * 3 + s] = 1
 
     return A
 
 
 def ravel(camera_params):
-    cp_ravel = np.empty((len(camera_params), 21), dtype=float)
+    cp_ravel = np.empty((len(camera_params), PARAM_CALIB_SIZE), dtype=float)
 
     for i in range(len(camera_params)):
         cp_ravel[i, :9] = camera_params[i]['extrinsics'][:3, :3].ravel()
@@ -91,7 +93,7 @@ def ravel(camera_params):
         cp_ravel[i, 13] = camera_params[i]['mtx'][1, 1]
         cp_ravel[i, 14] = camera_params[i]['mtx'][0, 2]
         cp_ravel[i, 15] = camera_params[i]['mtx'][1, 2]
-        cp_ravel[i, 16:] = camera_params[i]['dist'].ravel()
+        # cp_ravel[i, 16:] = camera_params[i]['dist'].ravel()
 
     return cp_ravel.ravel()
 
@@ -118,13 +120,15 @@ def optimize(n_cameras, n_points, camera_indices, point_indices,
     return res
 
 
-def store_results(results, experiment):
+def store_results(results, experiment, n_cameras):
     if not os.path.exists(DIR_STORE):
         os.mkdir(DIR_STORE)
 
+    calib_params_size = n_cameras * PARAM_CALIB_SIZE
+
     output = {
-        'params': results['x'][:84],
-        'points_3d': results['x'][84:]
+        'params': results['x'][:calib_params_size],
+        'points_3d': results['x'][calib_params_size:]
     }
 
     path = os.path.join(DIR_STORE, f'keypoints3d_{experiment}_ba.pkl')
@@ -145,7 +149,7 @@ if __name__ == '__main__':
         n_cameras = len(camera_params)
         n_points = points_3d.shape[0]
 
-        n = 21 * n_cameras + 3 * n_points
+        n = PARAM_CALIB_SIZE * n_cameras + 3 * n_points
         m = 2 * points_2d.shape[0]
 
         # Visualizing initial error error
@@ -157,7 +161,7 @@ if __name__ == '__main__':
         results = optimize(n_cameras, n_points, camera_indices,
                        point_indices, points_2d)
         
-        store_results(results, experiment)
+        store_results(results, experiment, n_cameras)
 
         visualize_error(results['x'], n_cameras, n_points, camera_indices,
                         point_indices, points_2d)
