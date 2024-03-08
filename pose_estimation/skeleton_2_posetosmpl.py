b4c4d0235f86715394eba419d07ef86275f0c64e
sandstorm12 <sandstormeatwo@gmail.com>
2024-03-08 04:05:30 +0330

    3D skeleton to pose_2_smpl formatter updated with the new 3D skeletons

--- pose_estimation/skeleton_2_posetosmpl.py
+++ pose_estimation/skeleton_2_posetosmpl.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import diskcache
 import numpy as np
 
@@ -7,7 +8,8 @@ from scipy.spatial.transform import Rotation
 from numpy.linalg import norm
 
 
-OUTPUT_DIR = './skeleton_numpypkl'
+DIR_INPUT = './keypoints_3d_ba'
+DIR_OUTPUT = './keypoints_3d_pose2smpl'
 
 
 def magnitude (v):
@@ -82,73 +84,59 @@ def rotate_z(points, angle):
     rotation_matrix = [[cos, -sin, 0], [sin, cos, 0], [0, 0, 1]]
     return [[sum([rotation_matrix[i][j] * point[j] for j in range(3)]) for i in range(3)] for point in points]
 
-# TODO: Needs heavy refactor
-def skeleton_2_numpypkl(output_dir, cache_process):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
 
-    for key in cache_process.keys():
-        if 'skeleton_3D_smooth' in key:
-            skeleton = np.array([keypoints[0]
-                                 for keypoints in cache_process[key]],
-                                dtype=float)
+def skeleton_2_numpypkl(path_input, dir_output, name):
+    with open(path_input, 'rb') as handle:
+        output = pickle.load(handle)
+
+    skeleton = output['points_3d']
+    skeleton = skeleton.reshape(-1, 2, 26, 3)
+    skeleton = skeleton[:, 0]
+
+    print(skeleton.shape)
+
+    x_range = np.max(skeleton[:, :, 0]) - np.min(skeleton[:, :, 0])
+    middle = np.copy(skeleton[:, 19])
+    y_range = np.max(skeleton[:, :, 1]) - np.min(skeleton[:, :, 1])
+    z_range = np.max(skeleton[:, :, 2]) - np.min(skeleton[:, :, 2])
+    g_range = max(x_range, y_range, z_range)
+
+    x_angle, y_angle, z_angle = line_angles(skeleton[0][11], skeleton[0][12])
+    for i in range(len(skeleton)):
+        for j in range(len(skeleton[i])):
+            skeleton[i][j] = (skeleton[i][j] - middle[i]) / g_range
+            skeleton[i][j][1] *= -1
+
+        skeleton[i] = np.array(rotate_y(skeleton[i], z_angle - 50))  # facing angle
+        skeleton[i] = np.array(rotate_z(skeleton[i], x_angle - 90))  # facing angle
+
+        # while True:
+        #     # x_angle, y_angle, z_angle = line_angles(skeleton[i][11], skeleton[i][12])
+        #     if z_angle - 90 < 5:
+        #         break
+        #     skeleton[i] = np.array(rotate_y(skeleton[i], z_angle - 90))  # facing angle
+
+        # while True:
+        #     # x_angle, y_angle, z_angle = line_angles(skeleton[i][11], skeleton[i][12])
+        #     if abs(x_angle - 180) < 5:
+        #         break
+        #     skeleton[i] = np.array(rotate_z(skeleton[i], x_angle - 180))  # facing angle
             
-            if skeleton.shape[0] != 200:
-                continue
 
-            x_range = np.max(skeleton[:, :, 0]) - np.min(skeleton[:, :, 0])
-            middle = np.copy(skeleton[:, 19])
-            y_range = np.max(skeleton[:, :, 1]) - np.min(skeleton[:, :, 1])
-            z_range = np.max(skeleton[:, :, 2]) - np.min(skeleton[:, :, 2])
-            g_range = max(x_range, y_range, z_range) / 2
-
-            for i in range(len(skeleton)):
-                for j in range(len(skeleton[i])):
-                    skeleton[i][j] = (skeleton[i][j] - middle[i]) / g_range
-                    skeleton[i][j][1] *= -1
-
-                x_angle, y_angle, z_angle = line_angles(skeleton[i][5], skeleton[i][6])
-
-                while True:
-                    x_angle, y_angle, z_angle = line_angles(skeleton[i][5], skeleton[i][6])
-                    if abs(z_angle - 80) < 1:
-                        break
-                    skeleton[i] = np.array(rotate_y(skeleton[i], 1))  # facing angle
-
-                x_angle, y_angle, z_angle = line_angles(skeleton[i][5], skeleton[i][6])
-
-                skeleton[i][20] = (skeleton[i][20] + skeleton[i][22]) / 2
-                skeleton[i][21] = (skeleton[i][21] + skeleton[i][23]) / 2
-
-                upper_scale = abs(skeleton[i][18][1] - skeleton[i][19][1])
-                lower_scale = abs(skeleton[i][12][1] - skeleton[i][16][1])
-                transform_scale = 3*lower_scale/(4*upper_scale)
-
-                for j in range(len(skeleton[i])):
-                    if skeleton[i][j][1] > 0:
-                        skeleton[i][j][1] = skeleton[i][j][1] * transform_scale * .9
-                
-                COEFF_1 = 1.5
-                skeleton[i][5][0] = skeleton[i][5][0] * COEFF_1
-                skeleton[i][6][0] = skeleton[i][6][0] * COEFF_1
-                skeleton[i][7][0] = skeleton[i][7][0] * COEFF_1
-                skeleton[i][8][0] = skeleton[i][8][0] * COEFF_1
-                # skeleton[i][9][0] = skeleton[i][9][0] * COEFF_1
-                # skeleton[i][10][0] = skeleton[i][10][0] * COEFF_1
-
-                COEFF_2 = 1.5
-                skeleton[i][13][0] = skeleton[i][13][0] * COEFF_2
-                skeleton[i][14][0] = skeleton[i][14][0] * COEFF_2
-                skeleton[i][15][0] = skeleton[i][15][0] * COEFF_2
-                skeleton[i][16][0] = skeleton[i][16][0] * COEFF_2
-
-            output_path = os.path.join(output_dir, f'{key}.npy')
-            with open(output_path, 'wb') as handle:
-                np.save(handle, skeleton)
+    output_path = os.path.join(dir_output, f'{name}.npy')
+    with open(output_path, 'wb') as handle:
+        np.save(handle, skeleton)
 
 
 if __name__ == "__main__":
-    cache = diskcache.Cache('../calibration/cache')
-    cache_process = cache.get('process', {})
+    if not os.path.exists(DIR_OUTPUT):
+        os.makedirs(DIR_OUTPUT)
 
-    skeleton_2_numpypkl(OUTPUT_DIR, cache_process)
+    names = os.listdir(DIR_INPUT)
+    for name in names:
+        path = os.path.join(DIR_INPUT, name)
+
+        print('Processing: {}'.format(path))
+    
+        skeleton_2_numpypkl(path, DIR_OUTPUT,
+                            name.split('.')[0])
