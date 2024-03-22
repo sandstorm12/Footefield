58b1a1794a6affd6e5d91eefbf4c6f3594dc6d2a
sandstorm12 <sandstormeatwo@gmail.com>
2024-03-23 00:42:47 +0330

    Registered point cloud sequential visualizer implemented

--- smpl/visualizer_pointcloud_seq_ba.py
+++ smpl/visualizer_pointcloud_seq_ba.py
@@ -4,6 +4,7 @@ sys.path.append('../')
 import os
 import cv2
 import time
+import glob
 import pickle
 import diskcache
 import numpy as np
@@ -11,6 +12,7 @@ import open3d as o3d
 
 
 DIR_PARAMS = '../pose_estimation/keypoints_3d_ba'
+DIR_PARMAS_FINETUNED = "./extrinsics_finetuned"
 COLOR_SPACE_GRAY = [0.203921569, 0.239215686, 0.274509804]
 
 
@@ -22,100 +24,35 @@ def get_images(cam, idx):
     return img_color, img_depth
 
 
-def get_pcd(cam, idx, params, cache):
+def get_pcd(cam, idx, extrinsics, cache):
     _, img_depth = get_images(cam, idx)
+    mtx, dist, _ = get_params_depth(cam, cache)
 
-    _, _, extrinsics_rgb = get_params(cam, params)
-    mtx, dist, extrinsics = get_params_depth(cam, cache)
-    extrinsics_finetuned = get_finetuned_extrinsics(cam)
-    extrinsics = np.matmul(extrinsics, extrinsics_rgb)
-    # extrinsics = np.matmul(extrinsics_finetuned, extrinsics)
-    
     img_depth = cv2.imread(img_depth, -1)
-
-    mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, mtx, (640, 576), cv2.CV_32FC2)
+    mapx, mapy = cv2.initUndistortRectifyMap(
+        mtx, dist, None, mtx, (640, 576), cv2.CV_32FC2)
     img_depth = cv2.remap(img_depth, mapx, mapy, cv2.INTER_NEAREST)
-
     depth = o3d.geometry.Image(img_depth)
 
-    intrinsics = o3d.camera.PinholeCameraIntrinsic(640, 576, mtx[0, 0], mtx[1, 1], mtx[0, 2], mtx[1, 2])
-
-    pcd = o3d.geometry.PointCloud.create_from_depth_image(depth, intrinsics, extrinsics)
-
-    pcd = pcd.transform(extrinsics_finetuned)
+    intrinsics = o3d.camera.PinholeCameraIntrinsic(
+        640, 576, mtx[0, 0], mtx[1, 1], mtx[0, 2], mtx[1, 2])
+    pcd = o3d.geometry.PointCloud.create_from_depth_image(
+        depth, intrinsics, extrinsics['base'])
+    pcd = pcd.transform(extrinsics['offset'])
 
     return pcd
 
 
-def get_finetuned_extrinsics(cam):
-    if cam == cam24:
-        extrinsics = np.array(
-            [
-                [1.00000000e00, 3.52090809e-26, 8.27180613e-25, -1.65436123e-24],
-                [3.52084522e-26, 1.00000000e00, 0.00000000e00, 0.00000000e00],
-                [-8.27180613e-25, 0.00000000e00, 1.00000000e00, 0.00000000e00],
-                [0.00000000e00, 0.00000000e00, 0.00000000e00, 1.00000000e00],
-            ]
-        )
-    elif cam == cam15:
-        extrinsics = np.array(
-            [
-                [0.99964627, -0.01086932, 0.02427319, -0.04616381],
-                [0.00990623, 0.9991724, 0.03945104, -0.08110659],
-                [-0.02468191, -0.03919663, 0.99892664, -0.03160174],
-                [0.0, 0.0, 0.0, 1.0],
-            ]
-        )
-    elif cam == cam14:
-        raise Exception("Unknown camera.")
-    elif cam == cam34:
-        extrinsics = np.array(
-            [
-                [0.99749669, -0.01552832, 0.06898719, -0.1396021],
-                [0.01539566, 0.99987847, 0.0024543, -0.01443389],
-                [-0.06901691, -0.00138605, 0.99761453, -0.01269228],
-                [0.0, 0.0, 0.0, 1.0],
-            ]
-        )
-    elif cam == cam35:
-        extrinsics = np.array(
-            [
-                [9.93827554e-01, -4.39298306e-03, 1.10848971e-01, -2.92642058e-01],
-                [4.51243087e-03, 9.99989477e-01, -8.26722131e-04, -1.38620371e-02],
-                [-1.10844173e-01, 1.32181755e-03, 9.93836919e-01, 5.66665409e-03],
-                [0.00000000e00, 0.00000000e00, 0.00000000e00, 1.00000000e00],
-            ]
-        )
-    else:
-        raise Exception("Unknown camera.")
-    
-    return extrinsics
+def load_finetuned_extrinsics():
+    extrinsics_finetuned = {}
+    for path in glob.glob(os.path.join(DIR_PARMAS_FINETUNED, '*')):
+        experiment = path.split('.')[-2].split('_')[-1]
+        with open(path, 'rb') as handle:
+            params = pickle.load(handle)
 
+        extrinsics_finetuned[experiment] = params
 
-def get_params(cam, params):
-    if cam == cam24:
-        idx_cam = 0
-    elif cam == cam15:
-        idx_cam = 1
-    elif cam == cam14:
-        raise Exception("Unknown camera.")
-    elif cam == cam34:
-        idx_cam = 2
-    elif cam == cam35:
-        idx_cam = 3
-    else:
-        raise Exception("Unknown camera.")
-
-    mtx = params[idx_cam]['mtx']
-    dist = params[idx_cam]['dist']
-    rotation = params[idx_cam]['rotation']
-    translation = params[idx_cam]['translation']
-
-    extrinsics = np.eye(4, dtype=float)
-    extrinsics[:3, :3] = rotation
-    extrinsics[:3, 3] = translation / 1000
-
-    return mtx, dist, extrinsics
+    return extrinsics_finetuned
 
 
 def get_params_depth(cam, cache):
@@ -133,7 +70,7 @@ def get_params_depth(cam, cache):
 
 def remove_outliers(pointcloud):
     _, ind = pointcloud.remove_statistical_outlier(
-        nb_neighbors=20,
+        nb_neighbors=5,
         std_ratio=.1)
     pointcloud = pointcloud.select_by_index(ind)
     
@@ -158,8 +95,8 @@ def visualize(cameras, params, cache):
 
     for i in range(1000):
         pcd = o3d.geometry.PointCloud()
-        for cam in cameras:
-            pcd += preprocess(get_pcd(cam, i, params, cache))
+        for idx_cam, cam in enumerate(cameras):
+            pcd += preprocess(get_pcd(cam, i, params[idx_cam], cache))
 
         geometry.points = pcd.points
         if i == 0:
@@ -188,26 +125,18 @@ if __name__ == "__main__":
     cameras = [
         cam24,
         cam15,
+        # cam14,
         cam34,
         cam35,
     ]
 
-    for file in os.listdir(DIR_PARAMS):
-        experiment = file.split('.')[-2].split('_')[-2]
-        # if experiment == 'a1':
-        #     continue
+    finetuned_extrinsics = load_finetuned_extrinsics()
 
-        print(experiment)
+    for file in os.listdir(DIR_PARAMS):
+        print(file)
+        experiment = file.split('.')[-2].split('_')[-2]
 
         file_path = os.path.join(DIR_PARAMS, file)
         print(f"Visualizing {file_path}")
-        
-        with open(file_path, 'rb') as handle:
-            output = pickle.load(handle)
 
-        poses = output['points_3d'].reshape(-1, 2, 26, 3)
-        params = output['params']
-
-        visualize(cameras, params, cache)
-    
-    
+        visualize(cameras, finetuned_extrinsics[experiment], cache)
